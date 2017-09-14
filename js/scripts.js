98ff192e9abdfc0f9db1c277785d6a1a0107bc98
Kimberly Bordon and Susannah Lowe <kimberly@>
2017-09-14 16:10:55 -0700

    Combine functions to determine other triangles types into one, and add if statement to execute said function.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,28 @@
 var isEquilateral = function (sideA, sideB, sideC) {
   if (sideA === sideB && sideA === sideC) {
     alert("working function");
-    $(".results").not("#equilateral").hide();
+    $("#isosceles, #scalene, #notTriangle").hide();
     $("#equilateral").show();
+    return 0;
+  } else {
+    return 1;
   }
 };
 
-var isIsosceles = function (sideA, sideB, sideC) {
+var otherTriangles = function (sideA, sideB, sideC) {
   if ((sideA === sideC && sideA !== sideB) || (sideA === sideB && sideB !== sideC) || (sideB === sideC && sideA !==sideB)) {
     alert("working function2");
+    $("#equilateral, #scalene, #notTriangle").hide();
     $("#isosceles").show();
+  } else if (sideA+sideB <= sideC && sideA  || sideB+sideC <= sideA || sideC+sideA <= sideB) {
+    $("#isosceles, #scalene, #equilateral").hide();
+    $("#notTriangle").show();
+  } else if (sideA !== sideC && sideA !== sideB && sideB !== sideC) {
+    alert("working function3");
+    $("#isosceles, #equilateral, #notTriangle").hide();
+    $("#scalene").show();
+  } else {
+    alert("There's an error. Find it! The triangles won't...");
   }
 };
 
@@ -22,15 +35,15 @@ var isIsosceles = function (sideA, sideB, sideC) {
 //   }
 // };
 
-var notTriangle = function (sideA, sideB, sideC) {
-  if (sideA+sideB <= sideC || sideB+sideC <= sideA || sideC+sideA <= sideB) {
-    alert("working function4");
-    $("#notTriangle").show();
-  } else if (sideA !== sideC && sideA !== sideB && sideB !== sideC) {
-    alert("working function3");
-    $("#scalene").show();
-  }
-};
+// var notTriangle = function (sideA, sideB, sideC) {
+//   if (sideA+sideB <= sideC || sideB+sideC <= sideA || sideC+sideA <= sideB) {
+//     alert("working function4");
+//     $("#notTriangle").show();
+//   } else if (sideA !== sideC && sideA !== sideB && sideB !== sideC) {
+//     alert("working function3");
+//     $("#scalene").show();
+//   }
+// };
 //function notTriangle may not need the else if, and maybe instead just an else to define a scalene.
 
 
@@ -42,9 +55,12 @@ $(document).ready(function() {
     var sideC = parseInt($("#sideC").val());
     alert("working");
 
-    isEquilateral(sideA, sideB, sideC);
-    isIsosceles(sideA, sideB, sideC);
-    notTriangle(sideA, sideB, sideC);
+    var otherCheck = 0;
+    otherCheck = isEquilateral(sideA, sideB, sideC);
+    if (otherCheck) {
+      otherTriangles(sideA, sideB, sideC);
+    }
+    // notTriangle(sideA, sideB, sideC);
 
 
     event.preventDefault();
