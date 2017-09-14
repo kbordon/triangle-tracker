342534fcbbdabcd6dd5b59036a32d71c349c0351
Kimberly Bordon and Susannah Lowe <susannah@>
2017-09-14 15:34:02 -0700

    Add notTriangle and scalene functions.

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,35 @@
 var isEquilateral = function (sideA, sideB, sideC) {
   if (sideA === sideB && sideA === sideC) {
     alert("working function");
+    $(".results").not("#equilateral").hide();
     $("#equilateral").show();
   }
 };
 
 var isIsosceles = function (sideA, sideB, sideC) {
-  if (sideA === sideC || sideA === sideB || sideB === sideC) {
+  if ((sideA === sideC && sideA !== sideB) || (sideA === sideB && sideB !== sideC) || (sideB === sideC && sideA !==sideB)) {
     alert("working function2");
     $("#isosceles").show();
   }
 };
+
+// var isScaleneorNo = function (sideA, sideB, sideC) {
+//   if (sideA !== sideC && sideA !== sideB && sideB !== sideC) {
+//     alert("working function3");
+//     $("#scalene").show();
+//   }
+// };
+
+var notTriangle = function (sideA, sideB, sideC) {
+  if (sideA+sideB <= sideC || sideB+sideC <= sideA || sideC+sideA <= sideB) {
+    alert("working function4");
+    $("#notTriangle").show();
+  } else if (sideA !== sideC && sideA !== sideB && sideB !== sideC) {
+    alert("working function3");
+    $("#scalene").show();
+  }
+};
+//function notTriangle may not need the else if, and maybe instead just an else to define a scalene.
 
 
 //Front-end Logic
@@ -24,7 +43,8 @@ $(document).ready(function() {
     alert("working");
 
     isEquilateral(sideA, sideB, sideC);
-    isIsosceles(sideA,sideB, sideC);
+    isIsosceles(sideA, sideB, sideC);
+    notTriangle(sideA, sideB, sideC);
 
 
     event.preventDefault();
